18cdb2fddc25002be4d257f023239ee6e5b989ed
anaol216 <atinafuanaol@gmail.com>
2025-09-20 21:32:19 +0300

    modified:   alx-project-0x00/interfaces/index.ts modified:   alx-project-0x00/pages/landing.tsx

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -11,16 +11,19 @@ const Landing: React.FC =  () => {
       <Button title="Small Rounded" styles="text-sm rounded-sm bg-blue-500 text-white" />
       <Button title="Small Medium" styles="text-sm rounded-md bg-green-500 text-white" />
       <Button title="Small Full" styles="text-sm rounded-full bg-red-500 text-white" />
+        <Button title="Small Full" styles="text-sm rounded-lg bg-purple-500 text-white" />
 
       {/* Medium buttons */}
       <Button title="Medium Rounded" styles="text-base rounded-sm bg-blue-500 text-white" />
       <Button title="Medium Rounded-md" styles="text-base rounded-md bg-green-500 text-white" />
       <Button title="Medium Full" styles="text-base rounded-full bg-red-500 text-white" />
+        <Button title="Medium Full" styles="text-base rounded-lg bg-purple-500 text-white" />
 
       {/* Large buttons */}
       <Button title="Large Rounded" styles="text-lg rounded-sm bg-blue-500 text-white" />
       <Button title="Large Rounded-md" styles="text-lg rounded-md bg-green-500 text-white" />
       <Button title="Large Full" styles="text-lg rounded-full bg-red-500 text-white" />
+      <Button title="Extra Large Full" styles="text-xl rounded-lg bg-purple-500 text-white" />
     </div>
     </div>
   )
