5c00dc9f2ce5cf2be46f348b4d488b3924d388a6
anaol216 <atinafuanaol@gmail.com>
2025-09-20 21:29:07 +0300

    new file:   alx-project-0x00/components/Button.tsx modified:   alx-project-0x00/interfaces/index.ts modified:   alx-project-0x00/pages/landing.tsx

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,10 +1,27 @@
 import Card from "@/components/Card"
+import Button from "@/components/Button"
 
 const Landing: React.FC =  () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
+      <div className="flex flex-col gap-4 p-8">
+      {/* Small buttons */}
+      <Button title="Small Rounded" styles="text-sm rounded-sm bg-blue-500 text-white" />
+      <Button title="Small Medium" styles="text-sm rounded-md bg-green-500 text-white" />
+      <Button title="Small Full" styles="text-sm rounded-full bg-red-500 text-white" />
+
+      {/* Medium buttons */}
+      <Button title="Medium Rounded" styles="text-base rounded-sm bg-blue-500 text-white" />
+      <Button title="Medium Rounded-md" styles="text-base rounded-md bg-green-500 text-white" />
+      <Button title="Medium Full" styles="text-base rounded-full bg-red-500 text-white" />
+
+      {/* Large buttons */}
+      <Button title="Large Rounded" styles="text-lg rounded-sm bg-blue-500 text-white" />
+      <Button title="Large Rounded-md" styles="text-lg rounded-md bg-green-500 text-white" />
+      <Button title="Large Full" styles="text-lg rounded-full bg-red-500 text-white" />
+    </div>
     </div>
   )
 }
